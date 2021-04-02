66c4aacda5b947fa4148c82bd0f290be91c7405d
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-04-02 17:16:20 +0530

    redux add with redux-basic page

---
+++ redux-basic.js
@@ -0,0 +1,37 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+ 
+  if(action.type === 'INC_Counter'){
+      return{
+          ...state,
+          counter: state.counter +1
+      }
+  }
+  if(action.type === 'Add_Counter'){
+    return{
+        ...state,
+        counter: state.counter + action.value1
+    }
+}
+
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// DispatchAction
+store.dispatch({type:'INC_Counter'});
+store.dispatch({type:'Add_Counter', value1: 10});
+
+console.log(store.getState());
+
+// Subscription 
