dbbf98d98dd45935d53c195e30e242bc125479ab
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2021-04-03 22:05:09 +0530

    redux reducer action usecase

---
+++ src/store/action.js
@@ -0,0 +1,7 @@
+export const Increment = 'INCREMENT';
+export const Decrement = 'DECREMENT';
+export const Add = 'ADD';
+export const Sub = 'SUB';
+export const Store_Result = 'STORE_RESULT';
+export const Delete_Result = 'DELETE_RESULT';
+
